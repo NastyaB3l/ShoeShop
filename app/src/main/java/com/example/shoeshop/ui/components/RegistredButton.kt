dd9617c651f18e3e3df041f758ee93015840c96e
Belova Anastasia <nastyb3lova@yandex.ru>
2025-12-15 13:11:49 +0300

    Добавлен компонент кнопки

---
+++ app/src/main/java/com/example/shoeshop/ui/components/RegistredButton.kt
@@ -0,0 +1,56 @@
+package com.example.shoeshop.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.shoeshop.R
+
+@Composable
+fun RegistrationButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = colorResource(R.color.Accent),
+            contentColor = colorResource((R.color.Background)),
+            disabledContentColor = colorResource((R.color.Background)),
+            disabledContainerColor = colorResource(R.color.Disable),
+        ),
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 4.dp,
+            pressedElevation = 8.dp
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.SignUp),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun RegistrationButton() {
+    RegistrationButton(
+        modifier = Modifier,
+        onClick = {}
+    )
+}
